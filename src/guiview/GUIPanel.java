32785bac89ea8544ab61cc8229c81d2b45591bba
KaiG24 <31518722+KaiG24@users.noreply.github.com>
2017-11-13 13:34:29 -0700

    More random commits

--- src/guiview/GUIPanel.java
+++ src/guiview/GUIPanel.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SpringLayout;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GUIPanel extends JPanel
 {
 	private GUIController appController;
@@ -23,6 +26,7 @@ public class GUIPanel extends JPanel
 		
 		setupPanel();
 		setupLayout();
+		setupListeners();
 	}
 	
 	private void setupPanel()
@@ -40,6 +44,21 @@ public class GUIPanel extends JPanel
 	
 	private void setupListeners()
 	{
+		firstButton.addActionListener(new ActionListener()
+				{
+			public void actionPerformed(ActionEvent click)
+			{
+				changeScreenColor();
+			}
+				});
+	}
+	
+	private void changeScreenColor() 
+	{
+		int red = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		int green = (int) (Math.random()* 256);
 		
+		this.setBackground(new Color(red, blue, blue));
 	}
 }
