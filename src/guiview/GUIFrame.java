36d92e5cd4e10b26e57e172a650da00f78dd7e9b
KaiG24 <31518722+KaiG24@users.noreply.github.com>
2017-11-13 12:58:13 -0700

    Making and moving the button

--- src/guiview/GUIFrame.java
+++ src/guiview/GUIFrame.java
@@ -7,18 +7,21 @@ public class GUIFrame extends JFrame
 {
 	
 	private GUIController appController;
+	private GUIPanel appPanel;
 	
 	public GUIFrame(GUIController appController)
 	{
 		super();
 		this.appController = appController;
+		this.appPanel = new GUIPanel(appController);
+		
 		setupFrame();
 	}
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
 		this.setTitle("Window title will go here");
 		this.setSize(500,500);
-		
 		this.setVisible(true);
 	}
 
--- src/guiview/GUIPanel.java
+++ src/guiview/GUIPanel.java
@@ -9,10 +9,37 @@ import javax.swing.SpringLayout;
 public class GUIPanel extends JPanel
 {
 	private GUIController appController;
+	private JButton firstButton;
+	private SpringLayout appLayout;
+	
 	
 	public GUIPanel(GUIController appController)
 	{
 		super();
 		this.appController = appController;
+		firstButton = new JButton("Click on the button");
+		appLayout = new SpringLayout();
+		
+		
+		setupPanel();
+		setupLayout();
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.BLUE);
+		this.setLayout(appLayout);
+		this.add(firstButton);
+	}
+	
+	private void setupLayout()
+	{
+		appLayout.putConstraint(SpringLayout.NORTH, firstButton, 115, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, firstButton, -140, SpringLayout.EAST, this);
+	}
+	
+	private void setupListeners()
+	{
+		
 	}
 }
